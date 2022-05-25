4f900f13c45e0abd5ebf7ebb4f474ef2b4fffd3e
Edward <edwarddn@gmail.com>
2022-05-25 19:17:50 -0300

    Update instala-tudo.sh

--- instala-tudo.sh
+++ instala-tudo.sh
@@ -196,24 +196,36 @@ removerFontsMs() {
 }
 
 instalarDocker() {
-	apt install -y ca-certificates curl gnupg lsb-release
 
-	mkdir -p /etc/apt/keyrings
- 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	if [ -f "/etc/apt/sources.list.d/docker.list" ]; then
+		echo 'O Docker já foi instalado'
+	else
 
-	apt update
-	apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+		apt install -y ca-certificates curl gnupg lsb-release
 
-	usermod -aG docker $SUDO_USER
+		mkdir -p /etc/apt/keyrings
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+		apt update
+		apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+		usermod -aG docker $SUDO_USER
+	fi
 }
 
 removerDocker() {
-	apt purge -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-	rm -rf /var/lib/docker
- 	rm -rf /var/lib/containerd
-	rm -rf /etc/apt/keyrings
-	rm -f /etc/apt/sources.list.d/docker.list
+
+	if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
+		echo 'O Docker não foi instalado'
+	else
+
+		apt purge -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+		rm -rf /var/lib/docker
+		rm -rf /var/lib/containerd
+		rm -rf /etc/apt/keyrings
+		rm -f /etc/apt/sources.list.d/docker.list
+	fi
 }
 
 instalarSublime() {
